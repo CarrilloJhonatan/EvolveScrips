9e3976e702360ce30883a1d51fa044d4a8de5c6e
JHONATAN STEVEN CARRILLO MEJIA <89527312+CarrilloJhonatan@users.noreply.github.com>
2024-10-08 23:01:26 -0500

    Version 2

--- CopyLogsRemote.bat
+++ CopyLogsRemote.bat
@@ -1,11 +1,12 @@
 @echo off
-REM Limpiar la carpeta de destino
+REM Limpiar la carpeta de destino (incluye archivos y subcarpetas)
 echo Limpiando C:\SantaCruz-Logs...
-del /Q "C:\SantaCruz-Logs\*"
+rd /S /Q "C:\SantaCruz-Logs"
+mkdir "C:\SantaCruz-Logs"
 
 REM Copiar los archivos de la carpeta de origen
 echo Copiando archivos de C:\LogsEvolve a C:\SantaCruz-Logs...
-xcopy "C:\LogsEvolve\*" "C:\SantaCruz-Logs\" /E /H /C /I
+xcopy "C:\LogsEvolve\*" "C:\SantaCruz-Logs\" /E /H /C /I /Y
 
 echo Operación completada.
 pause
